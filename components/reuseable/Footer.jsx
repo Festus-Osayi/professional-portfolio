bb096f08479635327a86342172617883a07b0908
Festus <fosayi@myseneca.ca>
2024-02-12 18:48:35 -0500

    added styling on the footer

--- components/reuseable/Footer.jsx
+++ components/reuseable/Footer.jsx
@@ -2,7 +2,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
 export default function Footer() {
   return (
-    <footer className="py-5 bg-[#0a101e] mt-[3rem]">
+    <footer className=" bg-[#0a101e] my-[2rem]">
       <Container>
         <p className="text-greyTextColor text-center">&copy; 2024. Developed by Festus Osayi. All right reserved.</p>
       </Container>

ee122310c8b54a597e54e79e1012ccfa67c97d27
Festus <fosayi@myseneca.ca>
2024-02-11 20:56:35 -0500

    added lazy loading functionality for ux

--- components/reuseable/Spinner.jsx
+++ components/reuseable/Spinner.jsx
@@ -2,8 +2,7 @@ import PacmanLoader from "react-spinners/PacmanLoader";
 export default function Spinner() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <PacmanLoader color="#36d7b7" loading={true} />
-      <p className="mt-3 text-xl">Please wait while the page is loading...</p>
+      <PacmanLoader color="#36d7b7" loading={true} size={40}/>
     </div>
   );
 }
